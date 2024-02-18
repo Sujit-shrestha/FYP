dc6a756e7669838a3640acb01173e79423df68fa
sujit <sujitshrestha0004@gmail.com>
2024-02-18 23:35:15 +0545

    hover effect

--- Client_Side/src/Packages/UI/Components/Dashboard/leftSideOptions.tsx
+++ Client_Side/src/Packages/UI/Components/Dashboard/leftSideOptions.tsx
@@ -1,10 +1,11 @@
-  export const Options   = (logo: string , name: string) => {
+  export const DashboardOptions   = (logo: string , name: string) => {
   const clickHandler = (name : string) =>{
     console.log(name + ' clicked')
   }
   return (
-    <div className="flex  p-3 hover:bg-slate-500 cursor-pointer" key={name} onClick={()=>clickHandler(name)}>
-      <img src={logo} alt="Logo" className="w-1/5 mr-3" />
+    <div className="flex  p-3 hover:text-orange-500  active:bg-slate-300  transition cursor-pointer select-none" key={name} onClick={()=>clickHandler(name)}>
+<img src={logo} alt="Logo" className="w-1/5 mr-3 hover:animate-pulse logo-img" style={{fill:"red"}} />
+      
         <span className="w-4/5">{name}</span>
     </div>
     
