70075ce7100b238b81ed8c2a853336133805b97b
sujit <sujitshrestha0004@gmail.com>
2024-02-20 06:48:46 +0545

    .

--- Client_Side/src/Packages/UI/Pages/Landing.tsx
+++ Client_Side/src/Packages/UI/Pages/Landing.tsx
@@ -1,8 +1,12 @@
-const Landing= ()=>{
+import { Link } from "react-router-dom";
+
+const Landing = () => {
   return (
     <>
       <h1>Landing page ...</h1>
+      <Link to="/dashboard"> Dashboard</Link>
+      <Link to="login"> Login</Link>
     </>
   );
-}
+};
 export default Landing;
