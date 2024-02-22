9ed5bdb96915364730e97f12277f85b10e466c60
sujit <sujitshrestha0004@gmail.com>
2024-02-22 09:12:51 +0545

    productCard in progress

--- Client_Side/src/Packages/UI/Components/productCard.tsx
+++ Client_Side/src/Packages/UI/Components/productCard.tsx
@@ -1,15 +1,31 @@
-import pic from './../../../assets/demopics/pic3.jpg'; 
+import pic from "./../../../assets/demopics/pic3.jpg";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ArrowButton } from "./Buttons/arrowButton";
 
-export const ProductCard = ()=>{
+export const ProductCard = () => {
+  return (
+    <div 
+      className="w-72 rounded overflow-hidden shadow-lg border-black"
+      style={{ border: " 2px solid blue" }}
+    >
+      <div id="topproductCard" className="flex justify-evenly">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="overflow-hidden">Name to render</div>
+        <ArrowButton />
+      </div>
 
-  return(
-<div className="w-72 rounded overflow-hidden shadow-lg ">
-  <img className="w-full" src={pic} alt="Card image" />
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">Card Title</div>
-   </div>
 
-</div>
+      <div id="midproductCard" className="flex">
+        data image
+        <div className="font-bold text-xl mb-2">Card Title</div>
+        <img className="w-full" src={pic} alt="Card image" />
+      </div>
 
+
+      <div id="bottomproductCard" className="flex ">4 buttons</div>
+    </div>
   );
-}
+};
