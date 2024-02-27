2d214c5a211fc60d9d559405e44f16c4c4a56779
sujit <sujitshrestha0004@gmail.com>
2024-02-27 05:48:20 +0545

    product car 80%

--- Client_Side/src/Packages/UI/Components/Buttons/productcardButton.tsx
+++ Client_Side/src/Packages/UI/Components/Buttons/productcardButton.tsx
@@ -1,8 +1,18 @@
-export const  productcardButton = (amount:number=450000 , color:string='green')=>{
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
+interface i {
+  amount? : number | null | undefined;
+  text? :string | null | undefined;
+  color? : string | null | undefined;
+  variant? : "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | null | undefined;
+}
 
-  return(
-    <div id="productcardButton" className="">
-
+export const ProductcardButton = ({ amount , color   , text = "" , variant = "secondary"}:i)  => {
+  return (
+    <div id="productcardButton">
+      <Button variant={variant} className={`text-black font-bold rounded-3xl bg${color}-500 text-lg w-50`}>
+        <Link to="/login" > {text} {amount} </Link>
+      </Button>
     </div>
   );
-}
+};
--- Client_Side/src/Packages/UI/Components/productCard.tsx
+++ Client_Side/src/Packages/UI/Components/productCard.tsx
@@ -1,15 +1,16 @@
 import pic from "./../../../assets/demopics/pic3.jpg";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ArrowButton } from "./Buttons/arrowButton";
+import { ProductcardButton } from "./Buttons/productcardButton";
 
-export const ProductCard = (name:string) => {
+export const ProductCard = (name: string) => {
   name = "Ram lal shresthafdsfds";
-  const truncatedName = name.length > 15 ? `${name.substring(0, 15)}...` :name ;
+  const truncatedName = name.length > 15 ? `${name.substring(0, 15)}...` : name;
 
   return (
     <div
       className="w-80  rounded overflow-hidden shadow-lg border-black text-white select-none"
-      style={{ border: " 2px solid blue" , backgroundColor: "#1A2233"}}
+      style={{ border: " 2px solid blue", backgroundColor: "#1A2233" }}
     >
       <div id="topproductCard" className="flex justify-space-between  p-2">
         <Avatar>
@@ -30,22 +31,25 @@ export const ProductCard = (name:string) => {
           id="text-midproductCard"
           className="flex flex-col  w-1/2 whitespace-normal " // Width set to 50%
         >
-         <span className="font-extrabold ">CRF 250</span>
-         <span>Model:</span>
-         <span>Kilometers:</span>
-         <span>Color:</span>
-         <span>No. of Bids:</span>
+          <span className="font-extrabold ">CRF 250</span>
+          <span>Model:</span>
+          <span>Kilometers:</span>
+          <span>Color:</span>
+          <span>No. of Bids:</span>
         </div>
         <div
           id="image-div"
-          className="w-1/2 flex justify-center rounded-lg overflow-hidden cursor-pointer" 
+          className="w-1/2 flex justify-center rounded-lg overflow-hidden cursor-pointer"
         >
-          <img className="max-w-full object-cover" src={pic} alt="Card image" /> 
+          <img className="max-w-full object-cover" src={pic} alt="Card image" />
         </div>
       </div>
 
-      <div id="bottomproductCard" className="flex p-2">
-        4 buttons
+      <div id="bottomproductCard" className="grid grid-cols-2 gap-1 my-5 p-3" >
+        <ProductcardButton  variant="secondary" amount={50000} text = "Rs. "  />
+        <ProductcardButton amount={50000} color="-orange" text = "Rs. " />
+        <ProductcardButton  color="-green" text = "Update Bid"/>
+        <ProductcardButton variant="link" amount={50000} color="white" ></ProductcardButton>
       </div>
     </div>
   );
